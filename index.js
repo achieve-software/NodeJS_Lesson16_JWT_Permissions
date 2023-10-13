f9d4c09891987bf524087d655470c336f1e295e0
Achieve-software <vtcbcdtyr@gmail.com>
2023-10-14 02:21:15 +0300

    log

--- index.js
+++ index.js
@@ -37,26 +37,51 @@ app.use(require("cookie-session")({ secret: process.env.SECRET_KEY }));
 // res.getModelList():
 app.use(require("./src/middlewares/findSearchSortPage"));
 
-// Login/Logout Control Middleware
-app.use(async (req, res, next) => {
+//Cookie: Login/Logout Control Middleware
+// app.use(async (req, res, next) => {
 
-  const Personnel = require('./src/models/personnel.model')
+//   const Personnel = require('./src/models/personnel.model')
 
+//   req.isLogin = false
+
+//   if (req.session?.id) {
+
+//       const user = await Personnel.findOne({ _id: req.session.id })
+
+//       // if (user && user.password == req.session.password) {
+//       //     req.isLogin = true
+//       // }
+//       req.isLogin = user && user.password == req.session.password
+//   }
+//   console.log('isLogin: ', req.isLogin)
+
+//   next()
+// })
+
+const jwt =require('jsonwebtoken')
+app.use((req, res, next) =>{
+  const auth = req.headers?.authorization || null
+  const accessToken = auth ? auth.split(' ')[1] : null
   req.isLogin = false
 
-  if (req.session?.id) {
-
-      const user = await Personnel.findOne({ _id: req.session.id })
-
-      // if (user && user.password == req.session.password) {
-      //     req.isLogin = true
-      // }
-      req.isLogin = user && user.password == req.session.password
-  }
-  console.log('isLogin: ', req.isLogin)
-
+    jwt.verify(accessToken, process.env.ACCESS_KEY, function(err, user) {
+        if (err) {
+            req.user = null
+            console.log('JWT Login: NO')
+        } else {
+            req.isLogin = true
+            req.user = user
+            // req.user = user.isActive ? user : null
+            console.log('JWT Login: YES')
+        }
+    })
   next()
 })
+
+
+
+
+
 
 // Routes:
 
